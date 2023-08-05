7b4fcfb68f6c1a708ef27b318ac0d7f7bbcb1b36
Kito-Java.23.7 <kito.job23@gmail.com>
2023-08-05 18:41:33 +0900

    [Add] Java-4thTheme

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.SimpleTimeZone;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,23 +11,19 @@ public class Main {
         Hunter reorio = new Hunter("Reorio", 20);
         List<Hunter> hunters = List.of(gonn, kirua, kurapika, reorio);
 
-        System.out.println("＜既存List＞");
         // 既存List表示
+        System.out.println("＜既存List＞");
         for (int i = 0; i < hunters.size(); i++) {
             System.out.println(hunters.get(i).getName());
         }
 
-        System.out.println("============");
-        System.out.println("＜新規List＞");
-
         // 18歳以下のハンターの名前を大文字で降順に出力する
+        System.out.println("＜新規List＞");
         hunters.stream()
                 .filter(hunter -> hunter.getAge() <= 18)
-                // ↓ この処理でstreamの要素の型がHunter型からString型になっている？
-                .map(hunter -> hunter.getName().toUpperCase())
+                .map(hunter -> hunter.getName().toUpperCase())   // この処理でstreamの要素の型がHunter型からString型になっている？
                 .sorted(Comparator.reverseOrder())
-                // ↓ 要素がString型のため、HunterクラスのgetName()は呼び出せない
-                .forEach(hunter -> System.out.println(hunter)); // メソッド参照：System.out::println
+                .forEach(hunter -> System.out.println(hunter));  // 要素がString型のため、HunterクラスのgetName()は呼び出せない
 
         // ⇒ Streamによる処理はList自体の値には影響を与えず、中間操作中はメソッドが実行される度に、
         // 　新規のListが生成されそこに操作後の値が格納されると思われる。
